8abe3c154d558abf482938c43cadbaed315c4184
michellejaeg <michelles82@gmail.com>
2014-01-13 16:15:44 -0800

    Slight change to SQL re: location table

--- file_traversal.py
+++ file_traversal.py
@@ -1,19 +1,17 @@
 import sqlite3
 import os
 
-
 def process_file(rootpath, path, f):
     conn = sqlite3.connect('my_database')
     c = conn.cursor()
+    id = c.lastrowid
     dirpath = os.path.join(path, f)
-    modified = os.stat(f)
-    rootid = rootpath.id
+    modified = os.stat(dirpath)[9]
+    rootid = c.execute('SELECT id From rootdir WHERE rootdir = (?)',(rootpath, ))
+    print (type(rootid))
     seen = 1
-    c.execute('''IF EXISTS(SELECT * FROM location WHERE file = f)
-                     UPDATE location SET (?, ?, ?, ?, ?, ?) WHERE file = f;
-                 ELSE
-                     INSERT INTO location VALUES (?, ?, ?, ?, ?, ?);''',
-                     (id, f, dirpath, modified, seen, rootid),(id, f, dirpath, modified, seen, rootid))
+    c.execute('REPLACE INTO location VALUES (?, ?, ?, ?, ?, ?)',
+                                            (id, f, dirpath, modified, seen, rootid))
 
 def delete_file(f):
     conn = sqlite3.connect('my_database')
@@ -21,36 +19,29 @@ def delete_file(f):
     cu.execute('DELETE FROM location WHERE file = f;')
 
 def main ():
-    # existed = os.path.exists("/Users/mswank/Documents/Dev/")
     conn = sqlite3.connect('my_database')
     curs = conn.cursor()
-    #if not existed:
-    curs.execute('DROP TABLE IF EXISTS rootdir;')
+    #curs.execute('DROP TABLE IF EXISTS location;')
     curs.execute('''
-                CREATE TABLE rootdir(
-                    id SERIAL PRIMARY KEY,
-                    rootdir CHARACTER VARYING(255))
-                ''')
-    curs.execute('DROP TABLE IF EXISTS location;')
-    curs.execute('''
-                CREATE TABLE location(
+                CREATE TABLE IF NOT EXISTS location(
                     id serial NOT NULL PRIMARY KEY,
                     filename character varying(128),
                     dirpath character varying(511),
                     modified timestamp without time zone,
-                    digest character(64),
                     seen boolean NOT NULL DEFAULT 0,
                     rootid INTEGER REFERENCES rootdir(id))
                 ''')
-    curs.execute('SELECT * FROM rootdir;')
+    curs.execute('SELECT rootdir FROM rootdir;')
     conn.commit()
     rootdir_rows = curs.fetchall()
-    #else:
-        #curs.execute('SELECT * FROM rootdir;')
-        #.rootdir_rows = curs.fetchall()
+
 
     for rootpath in rootdir_rows:
-        for path, dirs, files in os.walk("/Users/mswank/Documents/Dev"):
+        rootpath = str(rootpath)
+        rootpath = rootpath.replace(",", "")
+        rootpath = rootpath.strip("(, )")
+        rootpath = rootpath.strip("', '")
+        for path, dirs, files in os.walk(rootpath):
             for f in files:
                 process_file(rootpath, path, f)
 
