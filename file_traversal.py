ea1f942ff66089f4ed9ff4f8915ceecb8d79b0f9
michellejaeg <michelles82@gmail.com>
2014-02-25 22:40:36 -0800

    changes to insert statements, etc.

--- file_traversal.py
+++ file_traversal.py
@@ -3,44 +3,40 @@ import os, time
 
 new_files = []
 modified_files = {}
+conn = sqlite3.connect('my_database')
+c = conn.cursor()
+
 
 def process_file(rootpath, path, f):
-    conn = sqlite3.connect('my_database')
-    c = conn.cursor()
-    id = c.lastrowid
+    id = null
     dirpath = os.path.join(path, f)
     # Time and date that the file was last modified
     modified = (time.ctime(os.stat(dirpath).st_mtime))
-    c.execute('SELECT id FROM paths WHERE rootdir = (?)',(rootpath, ))
+    c.execute('SELECT id FROM paths WHERE rootdir = ?',(rootpath, ))
     list_of_rootids = c.fetchall()
     # Finds first tuple out of list, then first integer out of that tuple
     rootid = list_of_rootids[0][0]
     # Seen is now true
     seen = 1
-    c.execute('SELECT filename from location WHERE filename = (?)', (f,))
+    c.execute('SELECT filename from location WHERE filename = ?', (f,))
     old = c.fetchall()
-    if old == []:
+    # if old is empty
+    if not old:
+    # Add f to new_files
         new_files.append(f)
-    c.execute('SELECT modified from location WHERE modified = (?)', (modified,))
+    c.execute('SELECT modified from location WHERE modified = ?', (modified,))
     were_modified = c.fetchall()
     if len(were_modified) == 1:
         modified_files[f] = modified
-    c.execute('INSERT INTO location VALUES (?, ?, ?, ?, ?, ?)',
-                                            (id, f, dirpath, modified, seen, rootid))
-
+    c.execute('INSERT INTO location VALUES (?,?,?,?,?,?)', (id, f, dirpath, modified,
+                                                           seen, rootid))
 
 def delete_file(f):
-    conn = sqlite3.connect('my_database')
-    cu = conn.cursor()
-    cu.execute('DELETE FROM location WHERE file = f;')
+    f = f[0]
+    c.execute('DELETE FROM location WHERE filename = ?', (f,))
 
 def main ():
-    """
-
-    """
-    conn = sqlite3.connect('my_database')
-    curs = conn.cursor()
-    curs.execute('''
+    c.execute('''
                 CREATE TABLE IF NOT EXISTS location(
                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                     filename character varying(128),
@@ -49,9 +45,11 @@ def main ():
                     seen INTEGER DEFAULT 0,
                     rootid INTEGER REFERENCES paths(id))
                 ''')
-    curs.execute('SELECT rootdir FROM paths;')
-    conn.commit()
-    rootdir_rows = curs.fetchall()
+    c.execute('SELECT rootdir FROM paths;')
+
+    rootdir_rows = c.fetchall()
+
+
 
     for root_path in rootdir_rows:
         for path, dirs, files in os.walk(root_path[0]):
@@ -60,11 +58,11 @@ def main ():
 
 
     # Makes list of deleted files and delete them from database
-    curs.execute('SELECT * FROM location WHERE seen=0;')
-    file_rows = curs.fetchall()
-    print (file_rows)
-    for file in file_rows:
-        delete_file(file)
+    c.execute('SELECT filename FROM location WHERE seen=0;')
+    file_rows = c.fetchall()
+    print(file_rows)
+    #for file in file_rows:
+        #delete_file(file)
     deleted_files = [item for item in file_rows]
 
 
@@ -76,7 +74,12 @@ def main ():
 
     # Need to change seen to 0 again for next time?
     seen = 0
-    curs.execute('UPDATE location SET seen = 0')
+    #c.execute('UPDATE location SET seen = 0')
+
+
+
+    conn.commit()
+    c.close()
 
 
 if __name__ == "__main__":
