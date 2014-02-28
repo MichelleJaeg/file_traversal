b68f64a27c3dd134070a47988a5ac654f8f50389
michellejaeg <michelles82@gmail.com>
2014-02-27 19:00:16 -0800

    various updates

--- file_traversal.py
+++ file_traversal.py
@@ -6,33 +6,34 @@ modified_files = {}
 conn = sqlite3.connect('my_database')
 c = conn.cursor()
 
-
 def process_file(rootpath, path, f):
-    id = null
-    dirpath = os.path.join(path, f)
+    id = None
+    dir_path = os.path.join(path, f)
     # Time and date that the file was last modified
-    modified = (time.ctime(os.stat(dirpath).st_mtime))
+    modified = (time.ctime(os.stat(dir_path).st_mtime))
     c.execute('SELECT id FROM paths WHERE rootdir = ?',(rootpath, ))
     list_of_rootids = c.fetchall()
     # Finds first tuple out of list, then first integer out of that tuple
     rootid = list_of_rootids[0][0]
-    # Seen is now true
     seen = 1
     c.execute('SELECT filename from location WHERE filename = ?', (f,))
     old = c.fetchall()
-    # if old is empty
     if not old:
-    # Add f to new_files
         new_files.append(f)
     c.execute('SELECT modified from location WHERE modified = ?', (modified,))
     were_modified = c.fetchall()
-    if len(were_modified) == 1:
+    if not were_modified and not f in new_files:
         modified_files[f] = modified
-    c.execute('INSERT INTO location VALUES (?,?,?,?,?,?)', (id, f, dirpath, modified,
-                                                           seen, rootid))
+    c.execute('SELECT * FROM location WHERE filename = ?', (f,))
+    found = c.fetchall()
+    if found:
+        c.execute('UPDATE location SET modified = ?, seen = 1 WHERE filename = (?)', (modified, f))
+    else:
+        c.execute('INSERT INTO location VALUES (?,?,?,?,?,?)', (id, f, dir_path, modified,
+                                                               seen, rootid))
+    conn.commit()
 
 def delete_file(f):
-    f = f[0]
     c.execute('DELETE FROM location WHERE filename = ?', (f,))
 
 def main ():
@@ -45,39 +46,24 @@ def main ():
                     seen INTEGER DEFAULT 0,
                     rootid INTEGER REFERENCES paths(id))
                 ''')
-    c.execute('SELECT rootdir FROM paths;')
-
+    c.execute('SELECT rootdir FROM paths')
     rootdir_rows = c.fetchall()
-
-
-
     for root_path in rootdir_rows:
         for path, dirs, files in os.walk(root_path[0]):
             for f in files:
                 process_file(root_path[0], path, f)
-
-
-    # Makes list of deleted files and delete them from database
-    c.execute('SELECT filename FROM location WHERE seen=0;')
+    # Remove deleted files from database
+    c.execute('SELECT filename FROM location WHERE seen = 0')
     file_rows = c.fetchall()
-    print(file_rows)
-    #for file in file_rows:
-        #delete_file(file)
-    deleted_files = [item for item in file_rows]
-
-
+    deleted_files = [item[0] for item in file_rows]
+    for file in deleted_files:
+        delete_file(file)
     # Report information
     print ("The following files have been deleted:", deleted_files )
     print ("The following files have been added:", new_files)
     print ("The following files have been modified:", modified_files)
-
-
-    # Need to change seen to 0 again for next time?
-    seen = 0
-    #c.execute('UPDATE location SET seen = 0')
-
-
-
+    # Set variable 'seen' back to 0 for all rows
+    c.execute('UPDATE location SET seen = 0')
     conn.commit()
     c.close()
 
